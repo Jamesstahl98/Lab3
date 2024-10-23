68a4d8fa4127e41f2fdf9c7f3e11228a97cfa532
Jamesstahl98 <Jamesstahl556@gmail.com>
2024-10-23 11:15:40 +0200

    Added Question and QuestionPack classes

---
+++ Lab 3/Model/Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3.Model
+{
+    internal class Question
+    {
+        public Question(string query, string correctAnswer, string incorrectAnswerOne, string incorrectAnswerTwo, string incorrectAnswerThree)
+        {
+            Query = query;
+            CorrectAnswer = correctAnswer;
+            IncorrectAnswers = new string[3] { incorrectAnswerOne, incorrectAnswerTwo, incorrectAnswerThree };
+        }
+
+        public string Query { get; set; }
+        public string CorrectAnswer { get; set; }
+        public string[] IncorrectAnswers { get; set; }
+    }
+}

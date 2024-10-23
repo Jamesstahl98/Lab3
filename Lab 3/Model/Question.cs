a4bb2bb882136a346a292d3e9b124d6eb966776a
Jamesstahl98 <Jamesstahl556@gmail.com>
2024-10-23 13:54:05 +0200

    Imported FontAwesome.Sharp and added icons to MenuView and ConfiguratorView

--- Lab 3/Model/Question.cs
+++ Lab 3/Model/Question.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Lab_3.Model
+﻿namespace Lab_3.Model
 {
     internal class Question
     {

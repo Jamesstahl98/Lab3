6186f2f56140977c7853452a5cb1bd289c7cfd3b
Jamesstahl98 <Jamesstahl556@gmail.com>
2024-10-30 20:38:17 +0100

    EnumToIntConverter, Dialogs
    Comboboxes in dialogs is now correctly binded to difficulty

--- Lab 3/Converters/EnumToIntConverter.cs
+++ Lab 3/Converters/EnumToIntConverter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lab_3.Model;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -10,28 +11,14 @@ namespace Lab_3.Converters
 {
     public class EnumToIntConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter,
-                          System.Globalization.CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            Enum enumValue = default(Enum);
-            if (parameter is Type)
-            {
-                string valueString = value.ToString();
-                var subString = valueString.Substring(0, 0).ToUpper() + valueString.Substring(1);
-                enumValue = (Enum)Enum.Parse((Type)parameter, value.ToString());
-            }
-            return enumValue;
+            return (int)value;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter,
-                                  System.Globalization.CultureInfo culture)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            int returnValue = 0;
-            if (parameter is Type)
-            {
-                returnValue = (int)Enum.Parse((Type)parameter, value.ToString());
-            }
-            return returnValue;
+            return (Difficulty)value;
         }
     }
 }

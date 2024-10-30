df0ac9b8715158b7d9d8f36d30e201a1b4722d20
Jamesstahl98 <92599615+Jamesstahl98@users.noreply.github.com>
2024-10-30 16:09:45 +0100

    Create EnumToIntConverter.cs

---
+++ Lab 3/Converters/EnumToIntConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Lab_3.Converters
+{
+    public class EnumToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+                          System.Globalization.CultureInfo culture)
+        {
+            Enum enumValue = default(Enum);
+            if (parameter is Type)
+            {
+                string valueString = value.ToString();
+                var subString = valueString.Substring(0, 0).ToUpper() + valueString.Substring(1);
+                enumValue = (Enum)Enum.Parse((Type)parameter, value.ToString());
+            }
+            return enumValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+                                  System.Globalization.CultureInfo culture)
+        {
+            int returnValue = 0;
+            if (parameter is Type)
+            {
+                returnValue = (int)Enum.Parse((Type)parameter, value.ToString());
+            }
+            return returnValue;
+        }
+    }
+}

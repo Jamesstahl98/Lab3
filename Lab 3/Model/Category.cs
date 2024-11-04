75e4b43b292ef8f223a0c45f9443c7cb7bcdcc2a
Jamesstahl98 <Jamesstahl556@gmail.com>
2024-11-04 13:42:17 +0100

    Added common Dialog style + new dialog for importing questions

---
+++ Lab 3/Model/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3.Model
+{
+    internal class Category
+    {
+        public Dictionary<Enum, ObservableCollection<Question>> Questions { get; set; }
+
+
+    }
+}
---
+++ Lab 3/ViewModel/QuestionImporterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Lab_3.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3.ViewModel
+{
+    internal class QuestionImporterViewModel : ViewModelBase
+    {
+        
+    }
+}
